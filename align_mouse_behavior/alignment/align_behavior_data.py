Fix time calculation in plot_mouse_location to use recording-specific start time for accuracy

align_mouse_behavior/alignment/align_behavior_data.py
@@ -442,7 +442,7 @@ def plot_mouse_location(ax,data,rw_loc=0):
     min_val = np.nanmin(loc)
     max_val = np.nanmax(loc)
 
-    time = data["time"] - data["time"][0]
+    time = data["time"] - data["time"][data["recording"]][0]
 
     loc_dist = max_val - min_val
 